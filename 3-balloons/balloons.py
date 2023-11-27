daaba88e94f8ea826052ed11a4cfb316ae51b4f5
Kayle Dee <kdaniels24@basecampcodingacademy.org>
2023-11-27 13:29:25 -0600

    OOP do be done

---
+++ 3-balloons/balloons.py
@@ -0,0 +1,29 @@
+class BalloonTooFull(Exception):
+    pass
+
+
+class Balloon:
+    def pump(self):
+        self.amount += 3
+        if self.amount > self.capacity:
+            raise BalloonTooFull("Pop!")
+
+    def release(self):
+        self.amount -= 2
+        self.amount = max(0, self.amount)
+
+
+class SwordBalloon(Balloon):
+    def __init__(self):
+        self.amount = 0
+        self.capacity = 5
+        
+class DogBalloon(Balloon):
+    def __init__(self):
+        self.amount = 0
+        self.capacity = 7
+
+class TriforceBalloon(Balloon):
+    def __init__(self):
+        self.amount = 0
+        self.capacity = 11
